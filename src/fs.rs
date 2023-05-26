c91fbef26fa1deac5a88f3197ffff7373a7441bf
mazylol <landonp95@gmail.com>
2023-05-25 21:36:09 -0400

    simple test

--- src/fs.rs
+++ src/fs.rs
@@ -20,7 +20,7 @@ impl Config {
             fs::create_dir_all(&path.parent().unwrap()).unwrap();
             let mut file = File::create(&path).unwrap();
             let config = Config {
-                api_key: String::from(""),
+                api_key: String::from("REPLACE_ME"),
             };
             let json = serde_json::to_string_pretty(&config).unwrap();
             file.write_all(json.as_bytes()).unwrap();
@@ -28,5 +28,17 @@ impl Config {
         } else {
             return serde_json::from_reader(file.unwrap()).unwrap();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load() {
+        let config = Config::load();
+        println!("{}", config.api_key);
+
     }
 }
